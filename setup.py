ed707122b8b11360008d60b16af758a4f8401fde
teru <s.cheon@signate.co.jp>
2021-12-12 23:42:23 +0900

    upgrade urllib3

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ if sys.version_info[:2] < (3, 6):
     raise RuntimeError("Python version >= 3.6 required.")
 
 CLI_REQUIRES = ['click', 'tabulate', 'wget']
-SWAGGER_REQUIRES = ['urllib3 >= 1.26.4', 'six >= 1.10', 'certifi', 'python-dateutil']
+SWAGGER_REQUIRES = ['urllib3 >= 1.26.7', 'six >= 1.16', 'certifi', 'python-dateutil']
 
 setup(
     name='signate',
